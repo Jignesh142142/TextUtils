18cbf7544a6c471933c4bbb8dcda34d33ba71c6c
Jignesh142142 <jigneshkumar142@gmail.com>
2024-08-29 14:22:41 +0530

    jcmt

--- src/App.js
+++ src/App.js
@@ -39,22 +39,22 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
     <Alert alert={alert}/>
     <div className="container my-3">
-    <Switch>
+    {/* <Switch> */}
     {/* /users --> Component 1
         /users/home --> Component 2 */}
-          <Route exact path="/about">
-            <About mode={mode} />
-          </Route>
-          <Route exact path="/">
+          {/* <Route exact path="/about"> */}
+            {/* <About mode={mode} /> */}
+          {/* </Route> */}
+          {/* <Route exact path="/"> */}
             <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
-          </Route>
-    </Switch>
+          {/* </Route> */}
+    {/* </Switch> */}
     </div>
-    </Router>
+    {/* </Router> */}
     </> 
   );
 }
